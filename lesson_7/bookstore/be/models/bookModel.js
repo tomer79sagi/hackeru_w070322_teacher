a7910b000518456d8a19769e2b0e24054244790a
tomer79sagi <me@tomersagi.com>
2023-02-06 21:24:00 +0200

    Added JWT, JOI and Middleware

--- lesson_7/bookstore/be/models/bookModel.js
+++ lesson_7/bookstore/be/models/bookModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const JOI = require("joi");
 
 // 'currency': EUR, USD, ILS, etc...
 const BookSchema = mongoose.Schema({
@@ -9,6 +10,41 @@ const BookSchema = mongoose.Schema({
     currency: String,
     quantity: Number
 })
+
+BookSchema.loadClass(Val);
+
+// JOI Validations
+const baselineValidation = {
+    isbn: JOI.number().required(),
+    title: JOI.string().required().min(2).max(400),
+    description: JOI.string(),
+    price: JOI.number().required().min(1).max(200),
+    currency: JOI.string().required().min(3).max(3),
+    quantity: JOI.number().required()
+};
+
+// Post Validation, using 'spread', copying of the object to avoid changing the original version
+BookSchema.statics.validatePost = (obj) => {
+    return JOI.object({
+        ...baselineValidation, 
+        id: JOI.string().forbidden()
+    }).validate(obj, { abortEarly: false });
+}
+
+// Put Validation
+BookSchema.statics.validatePut = (obj) => {
+    return JOI.object({
+        ...baselineValidation
+    }).validate(obj, { abortEarly: false });
+}
+
+// Delete Validation
+BookSchema.statics.validateDelete = (obj) => {
+    return JOI.object({
+        id: JOI.string().forbidden(), 
+        isbn: JOI.number().required()
+    }).validate(obj, { abortEarly: false });
+}
 
 const BookModel = mongoose.model("BookModel", BookSchema, "books");
 
